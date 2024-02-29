ad4ecb8557fcfdc0141190bd74586195bb46eead
Breno Vergilio <bjvergilio47@gmail.com>
2024-02-28 20:26:48 -0400

    test: add test to check if isActive parameter is being passed in the right way

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -25,8 +25,36 @@ public class CategoryTest
     Assert.Equal(validData.Description, category.Description);
     Assert.NotEqual(default, category.Id);
     Assert.NotEqual(default, category.CreatedAt);
-    Assert.True(category.CreatedAt > datetimeBefore);
-    Assert.True(category.CreatedAt < datetimeAfter);
+    Assert.True(category.CreatedAt >= datetimeBefore);
+    Assert.True(category.CreatedAt <= datetimeAfter);
     Assert.True(category.IsActive);
+  }
+
+  [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+  [Trait("Domain", "Category - Aggregates")]
+  [InlineData(true)]
+  [InlineData(false)]
+  public void InstantiateWithIsActive(bool isActive)
+  {
+    var validData = new
+    {
+      Name = "category name",
+      Description = "category description"
+    };
+
+    var datetimeBefore = DateTime.Now;
+
+    var category = new DomainEntity.Category(validData.Name, validData.Description, isActive);
+
+    var datetimeAfter = DateTime.Now;
+
+    Assert.NotNull(category);
+    Assert.Equal(validData.Name, category.Name);
+    Assert.Equal(validData.Description, category.Description);
+    Assert.NotEqual(default, category.Id);
+    Assert.NotEqual(default, category.CreatedAt);
+    Assert.True(category.CreatedAt >= datetimeBefore);
+    Assert.True(category.CreatedAt <= datetimeAfter);
+    Assert.Equal(isActive, category.IsActive);
   }
 }
