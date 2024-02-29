d2b01cd4ea36cc6bcf08340d4b1af63eeef23fb7
Breno Vergilio <bjvergilio47@gmail.com>
2024-02-28 20:17:48 -0400

    feat: add category initial structure

---
+++ src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,10 @@
+namespace FC.Codeflix.Catalog.Domain.Entity;
+
+public class Category(string name, string description)
+{
+  public Guid Id { get; private set; } = Guid.NewGuid();
+  public string Name { get; private set; } = name;
+  public string Description { get; private set; } = description;
+  public bool IsActive { get; private set; } = true;
+  public DateTime CreatedAt { get; private set; } = DateTime.Now;
+}
