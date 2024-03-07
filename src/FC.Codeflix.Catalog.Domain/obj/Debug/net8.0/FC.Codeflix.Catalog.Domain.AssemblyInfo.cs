26d37f59466bd5d829668e4d4085538dcf423f2a
Breno Vergilio <bjvergilio47@gmail.com>
2024-03-06 22:08:58 -0400

    git: remove obj and bin files

--- src/FC.Codeflix.Catalog.Domain/obj/Debug/net8.0/FC.Codeflix.Catalog.Domain.AssemblyInfo.cs
+++ src/FC.Codeflix.Catalog.Domain/obj/Debug/net8.0/FC.Codeflix.Catalog.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FC.Codeflix.Catalog.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9ec4122510af55df1d798afedaa7bbafee8188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5129627f408489dc35cad6a6d0bb0b766a5bc04e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FC.Codeflix.Catalog.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FC.Codeflix.Catalog.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
