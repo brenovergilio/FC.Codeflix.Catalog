ece903cef864b4eaac37d96261710456fc02329a
Breno Vergilio <bjvergilio47@gmail.com>
2024-03-11 21:25:05 -0400

    feat: add validator to get category use case

--- tests/FC.Codeflix.Catalog.UnitTests/obj/Debug/net8.0/FC.Codeflix.Catalog.UnitTests.AssemblyInfo.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/obj/Debug/net8.0/FC.Codeflix.Catalog.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FC.Codeflix.Catalog.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42db443105d154379a7b7ed3574d3703e11bed05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68a0a46f87ee7a6a6a8c88a322d6fd4db7c0b38a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FC.Codeflix.Catalog.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FC.Codeflix.Catalog.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
